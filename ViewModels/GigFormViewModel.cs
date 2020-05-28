4a027461a85a1aae1dcbc58d586b5096c8b5314d
Elsayed Mohamed Elsayed Ali Leilla <elsayed.mohammed@cis.asu.edu.eg>
2020-05-28 18:01:01 +0200

    Create Gigs Form

---
+++ ViewModels/GigFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+
+
+
+        public string Time { get; set; }
+        public string Date { get; set; }
+
+        public int Genre { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
