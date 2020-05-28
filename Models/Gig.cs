7ccc1720ecfa62920bac648d41b8a4c4871af62b
Elsayed Mohamed Elsayed Ali Leilla <elsayed.mohammed@cis.asu.edu.eg>
2020-05-28 21:58:07 +0200

    Implementing Saving Gig

--- Models/Gig.cs
+++ Models/Gig.cs
@@ -8,11 +8,17 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        [Required]
+
         public ApplicationUser Artist { get; set; }
 
+        [Required]
+        public string ArtistId { get; set; }
 
         [Required]
+        public int GenreId { get; set; }
+
+
+
         public Genre Genre { get; set; }
 
         public DateTime DateTime { get; set; }
